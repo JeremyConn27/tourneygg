3434f013a15cd29dafb64c9f007fe8f11597a259
JeremyConn27 <Jconn27@gmail.com>
2020-07-01 22:20:01 -0400

    pushing what's working

--- tourney-gg/src/app/navigation/navigation.component.ts
+++ tourney-gg/src/app/navigation/navigation.component.ts
@@ -6,10 +6,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navigation.component.less']
 })
 export class NavigationComponent implements OnInit {
-
+  sideNav;
+  navItems: { label: string; routerLink: string}[] = [];
   constructor() { }
 
   ngOnInit(): void {
+    this.sideNav = document.querySelector('nav');
+    this.buildNavItems();
   }
 
+  buildNavItems() {
+    // TODO restrict access using user rights
+    this.navItems.push(
+      { label: 'Login', routerLink: 'login'},
+      { label: 'Home', routerLink: 'home'},
+      { label: 'Dashboard', routerLink: 'dashboard'},
+      { label: 'Tournaments', routerLink: 'tournaments'}
+    );
+  }
+
+  toggleSideNav() {
+    this.sideNav.classList.toggle('open');
+  }
 }
